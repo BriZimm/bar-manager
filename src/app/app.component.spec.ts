2d5dc1e09079aeae3db6c02c8216beafb5b0a23e
Brian Zimmerman <BriZimm@users.noreply.github.com>
2019-03-17 15:25:37 -0500

    Fix app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'barman'`, () => {
+  it(`should have as title 'Bar Manager'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('barman');
+    expect(app.title).toEqual('Bar Manager');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to barman!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Bar Manager!');
   });
 });
